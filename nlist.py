c8722650e71998c450736a4d7e2087412a622aa8
ops00 <42478424+ops00@users.noreply.github.com>
2018-10-16 22:35:43 +0300

    Update nlist.py

--- nlist.py
+++ nlist.py
@@ -4,13 +4,21 @@
 
 
 
-neglist = ["lakkoja", "kuolen", "kuoli", "surma", "surmasi", "surmayö", "päälaelleen", "syöpää", 
-"syöpä", "syövästä", "myrsky", "myrskyn", "kerjäävä", "konkurssi", "ampuminen", "ampumisesta", "epäillään",
-"epäillä", "epäilee", "kuolinyöstä", "kärsinyt", "sydänvaivoista", "kuolemaansa", "skandaaliottelu", "skandaaliottelussa",
-"kilpailukielto", "kilpailukiellon", "alkoholismista", "alkoholismi", "tuomittiin", "tuomittu", "peruutti",
-"kieltäytyi", "vaurioutui", "kolari", "joukkomurha", "joukkomurhasta", "iskee", "hirviö", "hirviömyrsky",
-"hurrikaani", "evakoitu", "todistaja", "vakoilun", "raahautuivat", "traagisesta", "turmasta", "tragiset",
-"haaste", "floppi", "riehuminen", "itsemurha", "laittomasti", "väkivaltaa", "pettämistä", "yt-neuvottelut", "yt-neuvottelu",
-"vähentää", "työvoimapula", "huume", "huumeet", "doping", "terroristi", "pettymys", "kiukkuista", 
-"itku", "rahariita", "epäilyllä", "viinakin", "tulipalo", "kärjistyi", "kohusta", "kohuun", "kateellinen",
-"vihainen", "raivo", "raiskaus", "huono", "huonoa", "pelkää", "ahne", "luminen", "pimeä", "talvi", "kuollut"]
+neglist = ['lakkoja','kuolen','kuoli','surma','surmasi','surmayö','päälaelleen','syöpää', 
+'syöpä','syövästä','myrsky','myrskyn','kerjäävä','konkurssi','ampuminen','ampumisesta','epäillään',
+'epäillä','epäilee','kuolinyöstä','kärsinyt','sydänvaivoista','kuolemaansa','skandaaliottelu','skandaaliottelussa',
+'kilpailukielto','kilpailukiellon','alkoholismista','alkoholismi','tuomittiin','tuomittu','peruutti',
+'kieltäytyi','vaurioutui','kolari','joukkomurha','joukkomurhasta','iskee','hirviö','hirviömyrsky',
+'hurrikaani','evakoitu','todistaja','vakoilun','raahautuivat','traagisesta','turmasta','tragiset',
+'haaste','floppi','riehuminen','itsemurha','laittomasti','väkivaltaa','pettämistä','yt-neuvottelut','yt-neuvottelu',
+'vähentää','työvoimapula','huume','huumeet','doping','terroristi','pettymys','kiukkuista', 
+'itku','rahariita','epäilyllä','viinakin', 'tulipalo','kärjistyi','kohusta','kohuun','kateellinen',
+'vihainen','raivo','raiskaus', 'huono', 'huonoa', 'pelkää', 'ahne', 'luminen','pimeä','talvi', 'kuollut','romahti',
+'purskahti','itkuun','pöyristyttää','peruttu','haitta','säälimättömän','satuttaa','saalistajan','uhriksi','pedoksi',
+'poliisi','huutava','pula','Joukkotappelu','epäilee','tapon','karmiva','sähellys','irvaili','sääliä','petkuttaja','väliintulo',
+'vahingoittanut','tuhoamaan','uhmasi','myrskyä','roihahti','pelottavaa','haittasi','kuoli','rynnäkkökiväärin',
+'luotiin','syyttäjä','vaatii','kapteeniluutnanttia','vastuuseen','kuolemasta','ruumis','paloiteltiin','loppuelämän',
+'haitta,','saalistajan','uhriksi','pedoksi','epäilee','tapon','yrityksestä','pois','kesken','pelastuslaitos','vaatii',
+'ehdollista','vankeutta','törkeästä','rahanpesusta','kuolleena','rikosta','kuollut','hyllylle','absurdi','väite',
+'vangitun','vangittuna','muoviveroa','vero','pahoinpitelystä','operaatiosta','ambulanssin','Puolustusvoimien',
+'ruumis','kieltolappu']
