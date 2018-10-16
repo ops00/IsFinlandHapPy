f8eb1b356ec5e5e68855bf7408967a58cc184275
ops00 <42478424+ops00@users.noreply.github.com>
2018-10-16 22:36:06 +0300

    Update plist.py

--- plist.py
+++ plist.py
@@ -4,11 +4,14 @@
 
 
 
-poslist = ["valonpilkahdus", "siistinä", "siisti", "aurinko", "helle", "syyttömänä", "syytön",
-"puhkesivat", "kosinta", "eläin", "kissa", "koira", "trendikkäistä", "trendikäs", "seksitilaston", "seksiä",
-"tiesithän", "suosionosoituksiin", "erikoisversion", "muodista", "muoti", "naimisiin", "avio-onnesta",
-"avio-onni", "onni", "suosikkisi", "suosikki", "kohuttu", "muotivillitys", "söpö", "suloinen", "uutuuksia",
-"huikea", "kaunis", "herkullisin", "arkiruoka", "helppo", "kiva", "parhaat", "komeilee", "hassuttelu",
-"niksit", "iloinen", "hymy", "hyvää", "joulu", "positiivisesti", "rakastaa", "jännä", "jumalainen", "jalo",
-"ihana", "nätti", "kekseliäs", "värikäs", "kesä", "helläsydämminen", "empaattinen", "optimistinen", 
-"älykäs", "aurinkoinen", "rohkea", "uskomaton", "vikkelä", "voittoputki"]
+poslist = ['valonpilkahdus','siistinä','siisti','aurinko','helle','syyttömänä','syytön',
+'puhkesivat','kosinta','eläin','kissa','koira','trendikkäistä','trendikäs','seksitilaston','seksiä',
+'tiesithän','suosionosoituksiin','erikoisversion','muodista','muoti','naimisiin', 'avio-onnesta',
+'avio-onni','onni','suosikkisi','suosikki','kohuttu','muotivillitys','söpö','suloinen','uutuuksia',
+'huikea','kaunis','herkullisin','arkiruoka','helppo','kiva','parhaat','komeilee','hassuttelu',
+'niksit','iloinen','hymy','hyvää','joulu','positiivisesti','rakastaa','jännä','jumalainen', 'jalo',
+'ihana','nätti','kekseliäs','värikäs','kesä','helläsydämminen','empaattinen','optimistinen', 
+'älykäs','aurinkoinen','rohkea','uskomaton','vikkelä','voittoputki','upea','nousu','mestari','vihdoin',
+'voitto','mullisti','elämän','upeaa','pukuloistoa','vilisevällä','ensi-illassa','suurena','persoonana','hieno',
+'karisman','kaupittelija,','bisnesnero,','sankari','onnistui','huippuohjastaja','saavutti','harvinaisen',
+'merkkipaalun','upeat','keksisitkö','harrastaa','mullistavat']
